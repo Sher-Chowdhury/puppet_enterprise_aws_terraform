57c52e1c40058172e6b3cb0e97f6e3b9843ed9e0
Sher Chowdhury <sher.chowdhury@gmail.com>
2017-05-26 12:23:00 +0100

    wip

--- main.tf
+++ main.tf
@@ -10,6 +10,8 @@ provider "aws" {
 # the "centos.org" owner id is: '679593333241'
 # aws ec2 describe-images --filters "Name=root-device-type,Values=ebs" "Name=name,Values=CentOS Linux 6 x86_64*"
 # aws ec2 describe-images --image-ids ami-051b1563
+# monolithic install instance type recommendation (up to 4000 nodes):  c4.4xlarge (16 cores) but I recommend c4.8xlarge (36 cores) so you can run more jvm instances, handy for big mcollective runs
+# https://docs.puppet.com/pe/latest/sys_req_hw.html#monolithic-installation-hardware-requirements
 resource "aws_instance" "puppet_enterprise" {
    ami = "ami-051b1563"
    instance_type = "t2.micro"
